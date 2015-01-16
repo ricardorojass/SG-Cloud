c278f2e9c2ab77d380cd1c5c55da90d226f47df5
Ricardo Rojas <reddrum7@gmail.com>
2015-01-16 18:52:40 -0500

    Create new version

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -7,11 +7,24 @@ class VersionsController < ApplicationController
 
   def create
     @document = Document.find(params[:document_id])
-    @version  = @document.versions.create(versions_params)
-    #@document.update(current_version: @version)
-    
+    @version  = @document.versions.build(versions_params)
+    @version.number = @document.versions.count + 1 
+    @version.save
+    response = Unirest.post "https://www.googleapis.com/drive/v2/files?convert=true", 
+      headers:{"Authorization" => "Bearer #{session[:access_token]}", "Content-Type" => "application/json"}, 
+      parameters: {title: "doc-#{@version.id}", mimeType: "application/vnd.google-apps.document" }.to_json
 
-    redirect_to document_path(@document)
+      drive_id = response.body["id"]
+      @version.docdrive_id = drive_id
+      @version.document_id = @document.id
+
+    if @version.save
+      @version.document_id = @document.id
+      #@document.update(current_version: @version)
+      redirect_to document_path(@document)
+    else
+      render :new
+    end
   end
 
   def destroy
